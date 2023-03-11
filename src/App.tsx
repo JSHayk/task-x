3e7b1cf1ea3d6d8cbd16857b0387af8105af4a80
JSHayk <jshaykdeveloper@gmail.com>
2023-03-11 21:01:33 +0400

    added base logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,67 @@
-import React, { useState } from "react";
-import { MoonIcon } from "./icons/Moon";
-import logo from "./logo.svg";
+import React, {memo, useCallback, useEffect, useRef, useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  function toggleDarkMode() {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      localStorage.setItem("theme", "light");
-      document.documentElement.classList.remove("dark");
-    } else {
-      localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    }
-  }
-
-  return (
-    <div className="flex flex-col min-h-screen text-center bg-white dark:bg-gray-700">
-      <nav className="flex items-center justify-end h-16 px-4">
-        <button
-          className="flex p-2 text-black rounded hover:text-yellow-400 dark:text-white focus:outline-none hover:bg-gray-500 dark:hover:bg-gray-500 dark:hover:text-yellow-400"
-          onClick={toggleDarkMode}
-        >
-          <MoonIcon></MoonIcon>
-        </button>
-      </nav>
-      <header className="flex flex-col items-center justify-center flex-grow text-2xl text-gray-700 dark:text-white">
-        <img src={logo} className="animate-spin-slow h-72" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p className="my-8">
-          <button
-            className="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-teal-600 border border-transparent rounded-md shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+interface ITags {
+  id: number;
+  name: string;
+  color: string;
 }
 
-export default App;
+const App = () => {
+  const [tags, setTags] = useState<ITags[] | null>(null)
+  const [val, setVal] = useState<string>("");
+  const [isUpdated, setIsUpdated] = useState<boolean>(false);
+  const updateRef = useRef<boolean>(true);
+
+  const searchTags = useCallback(() => {
+    // setTags(prev => {
+    //   // return prev?.filter(item => item.id === )
+    // })
+  }, []);
+
+  useEffect(() => {
+    if (!updateRef.current) {
+      if (val.length === 0) {
+        updateRef.current = true;
+      }
+    } else {
+      updateRef.current = false;
+      return;
+    }
+    if (val.length === 0) {
+      setIsUpdated(false);
+    } else {
+      setIsUpdated(true);
+    }
+  }, [val]);  
+
+  useEffect(() => {
+    const savedTags = JSON.parse(localStorage.getItem("tags")!);
+    if (savedTags) {
+      setTags(savedTags);
+    }
+  }, []);
+  
+  return (
+    <div>
+      <input type="text" onChange={e => {
+        setVal(e.target.value);
+      }} />
+     {
+      isUpdated &&
+        <ul className='tags-list'>
+          {
+            tags?.map(item => {
+              const {id, name, color} = item;
+              return <li key={id}>
+                <div  className='tag-color'>{name}</div>
+              </li>
+            })
+          }
+        </ul>
+     }
+    </div>
+    
+  )
+}
+
+export default memo(App);
